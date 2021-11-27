9650aa61e70cdca18fab9a9763a0a0801bdb599b
juanpms2 <jpabloms2@hotmail.com>
2021-11-27 19:39:54 +0100

    resolved exercises TS

--- 03-javascript-typescript/playground-ts/src/index.ts
+++ 03-javascript-typescript/playground-ts/src/index.ts
@@ -1,30 +1,9 @@
-const elements = [0, 1, false, 2, "", 3];
-
-const compact = (arg) => {
-	if (typeof arg === "object" && !Array.isArray(arg) && arg) {
-		let newObj = {};
-		for (const key in arg) {
-			arg[key] ? (newObj[key] = arg[key]) : false;
-		}
-		return newObj;
-	}
-	if (Array.isArray(arg)) {
-		return arg.filter((item) => item);
-	}
-	return arg;
+const swap = <T, U>(arg1: T, arg2: U): [U, T] => {
+	return [arg2, arg1];
 };
 
-console.log(compact(123)); // 123
-console.log(compact(null)); // null
-console.log(compact([0, 1, false, 2, "", 3, null, undefined])); // [1, 2, 3]
-console.log(compact({})); // {}
-console.log(
-	compact({
-		price: 0,
-		name: "cloud",
-		altitude: NaN,
-		taste: undefined,
-		isAlive: false,
-		isNull: null,
-	})
-); // {name: "cloud"}
+let age: number, occupation: string;
+
+[occupation, age] = swap(39, "Placement officer");
+console.log("Occupation: ", occupation);
+console.log("Age: ", age);
